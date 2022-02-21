f8f0759b81e17783bb984871c2061e3e3cb659bc
tan4786 <99185190+tan4786@users.noreply.github.com>
2022-02-21 14:09:01 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,70 +1,61 @@
-const timer = document.getElementById('stopwatch');
+'use strict';
+{   
+    var time = 0;
+    var timerLabel = document.getElementById('timerLabel');
+    var startBtn = document.getElementById('startBtn');
+    var stopBtn = document.getElementById('stopBtn');
+    var resetBtn = document.getElementById('resetBtn');
+    var id;  // setTimeoutから返される値を入れる変数
 
-var day = 0;
-var hr = 0;
-var min = 0;
-var sec = 0;
-var stoptime = true;
+    // STARTボタン
+    function start() {
+        // スタートボタンを押せないようにする（これをしないと何回もスタートボタン押せてしまう）
+        startBtn.disabled = true;
+        // timeをsetTimeoutで設定したミリ秒ごとに1プラスする
+        time++;  
 
-function startTimer() {
-  if (stoptime == true) {
-        stoptime = false;
-        timerCycle();
-    }
-}
-function stopTimer() {
-  if (stoptime == false) {
-    stoptime = true;
-  }
-}
-function resetTimer() {
-  day = 0; hr = 0;min = 0;sec = 0;
-}
+        // 1.2.3.4桁
+        var min = Math.floor(time/10/10/10)
+        var sec = Math.floor(time/10/10);
+        var mSec = Math.floor(time/10);
+        var mir = time % 10;
+        // 分・秒・ミリ秒が１桁の場合は、先頭に０をつける
+        if (min < 10) min = min;
+        if (sec >= 60) sec = sec % 10; // 秒が10秒以上になった場合の処理（10になったら0になる）
+        if (sec < 10) sec = sec; 
+        if (mSec >= 10) mSec = mSec % 10
+        if (mSec < 10) mSec = mSec;
+        if (mir < 10) mir = mir;
 
-function timerCycle() {
-    if (stoptime == false) {
-    sec = parseInt(sec);
-    min = parseInt(min);
-    hr = parseInt(hr);
-    day = parseInt(day);
-
-    sec = sec + 1;
-
-    if (sec == 10) {
-      min = min + 1;
-      sec = 0;
-    }
-    if (min == 10) {
-      hr = hr + 1;
-      min = 0;
-      sec = 0;
-    }
-    if (hr == 10) {
-      day = day + 1;
-      hr = 0;
-      min = 0;
-      sec = 0;
+        // timerLabelを更新
+        timerLabel.innerHTML = min + ':' + sec + ':' + mSec + ':' + mir;
+        // setTimeoutでstart関数をループさせるイメージ？
+        id = setTimeout(start, 100);
     }
 
-    if (sec < 10 || sec == 0) {
-      sec = sec;
-    }
-    if (min < 10 || min == 0) {
-      min = min;
-    }
-    if (hr < 10 || hr == 0) {
-      hr = hr;
-    }
-    if (day < 10 || day == 0) {
-      day = day;
+    // STOPボタン
+    function stop() {
+        // 停止する
+        clearTimeout(id);
+        // スタートボタンを押せるようにする
+        startBtn.disabled = false;
+
     }
 
-    timer.innerHTML = day + ':' + hr + ':' + min + ':' + sec;
+    // RESETボタン
+    function reset() {
+        // 停止する
+        clearTimeout(id);
+        // タイムを0に戻す
+        time = 0;
+        // タイマーラベルをリセット
+        timerLabel.innerHTML = '0:0:0:0';
+        // スタートボタンを押せるようにする
+        startBtn.disabled= false;
+    }
 
-    setTimeout("timerCycle()", 100);
-  }
-}
-
-function resetTimer() {
-    timer.innerHTML = '0:0:0:0';
+    // クリックした時の処理
+    startBtn.addEventListener('click', start, false); // STARTボタン
+    stopBtn.addEventListener('click', stop, false); // STOPボタン
+    resetBtn.addEventListener('click', reset, false); // RESETボタン
 }
